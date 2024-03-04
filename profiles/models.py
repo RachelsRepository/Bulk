ab31b2a00ecc36c629c093c3d6ed484fcb2d6758
Rachel Hayden <rachelhayden@Rachels-MacBook-Pro-2.local>
2024-03-03 16:07:39 -0800

    regex update

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
